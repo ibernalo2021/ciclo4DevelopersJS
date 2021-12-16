88df59dd17f1e8af164b86fefc5e237fa2cd63ed
Arley Rivas G <arrivasg@unipacifico.edu.co>
2021-12-16 01:59:57 -0500

    Pendiente validar correo desde models user

--- index.ts
+++ index.ts
@@ -6,26 +6,67 @@ const main = async () => {
     await conectarBD();
     // CREAR UN USUARIO
     await UserModel.create({
-        correo:"arri@uni.com",
-        identificacion:'56416',
-        nombre:'wilbert',
-        apellido:'Rivas Granado',
-        rol:Enum_Rol.administrador,
+        correo: "carmeng@fdgd.com.co",
+        identificacion: '144156416',
+        nombre: 'carmensa',
+        apellido: 'Granado 2',
+        rol: Enum_Rol.lider,
     })
-    .then((u)=>{
-        console.log('usuario creado ', u);
-    }).catch((e)=>{
-        console.log('error creado el usuario ', e);
-    });
+        .then((u) => {
+            console.log('usuario creado ', u);
+        }).catch((e) => {
+            console.log('error creado el usuario ', e);
+        });
 
     //OBTENER LOS USUARIOS
-    await UserModel.find({identificacion:'1111807577'}) //busqueda especifica
-        .then((u)=>{
-            console.log('usuario' , u);
-        })
-        .catch((e)=>{
-            console.error('error obteniendo los usuarios', e);
-        });
+    // await UserModel.find() //busqueda especifica
+    //     .then((u)=>{
+    //         console.log('usuario' , u);
+    //     })
+    //     .catch((e)=>{
+    //         console.error('error obteniendo los usuarios', e);
+    //     });
+
+    //OBTENER UN USUARIO
+    // await UserModel.find({identificacion:'1111807577'}) //busqueda especifica
+    // .then((u)=>{
+    //     console.log('usuario' , u);
+    // })
+    // .catch((e)=>{
+    //     console.error('error obteniendo los usuarios', e);
+    // });
+
+    //OBTENER UN USUARIO
+    // await UserModel.findOne({identificacion:'111807577'}) //busqueda especifica
+    // .then((u)=>{
+    //     console.log('usuario encontrado: ' , u);
+    // })
+    // .catch((e)=>{
+    //     console.error('error al buscar usuario: ', e);
+    // });
+
+    // ACTUALIZAR UN USARIO
+    // await UserModel.findOneAndUpdate(
+    //     { identificacion: '56416' },
+    //     {
+    //         nombre: 'Wilbert',
+    //         apellido: 'Granado Rivas',
+    //     }
+    // ).then((u)=>{
+    //     console.log('Usuario Actualizado: ', u);
+    // }).catch((e)=>{
+    //     console.log('Error al actualizar usuario ' ,  e)
+    // });
+
+
+    //ELIMINAR UN USUARIO
+    // await UserModel.findOneAndDelete({
+    //     correo: 'arri@uni.com'
+    // }).then((u) => {
+    //     console.log('usuario eliminado: ', u);
+    // }).catch((e) => {
+    //     console.error('Error al eliminar usuario ', e);
+    // });
 };
 
 main();
--- models/user.ts
+++ models/user.ts
@@ -1,5 +1,5 @@
 import { Schema, model } from 'mongoose';
-import { Enum_Rol } from './enums';
+import { Enum_EstadoUsuario, Enum_Rol } from './enums';
 
 
 
@@ -9,12 +9,26 @@ interface User {
     nombre: string;
     apellido: string;
     rol: Enum_Rol;
+    estado: Enum_EstadoUsuario;
 }
 
 const userSchema = new Schema<User>({
     correo: {
         type: String,
         required: true,
+        unique: true,
+        validate: {
+            validator: (email) => {
+                if (email.includes('@') && email.includes('.')) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            },
+            message:'El formato del correo electronico está malo.',
+
+        },
     },
     identificacion: {
         type: String,
@@ -32,8 +46,13 @@ const userSchema = new Schema<User>({
     rol: {
         type: String,
         required: true,
-        enum: ['Estudiante', 'Líder', 'Administrador'],
+        enum: Enum_Rol,
     },
+    estado: {
+        type: String,
+        enum: Enum_EstadoUsuario,
+        default: Enum_EstadoUsuario.pendiente,
+    }
 });
 
 const UserModel = model('user', userSchema);
