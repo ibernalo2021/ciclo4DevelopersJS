9623c47e43d9039d676c2be2393686c1d020439f
Arley Rivas G <arrivasg@unipacifico.edu.co>
2021-12-20 21:24:55 -0500

    Frontend con consultas a usuarios ok

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import PrivateLayout from 'layouts/PrivateLayout';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { UserContext } from 'context/userContext';
+import { ApolloProvider, ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import Index from 'pages/Index';
+import Page2 from 'pages/Page2';
+import IndexCategory1 from 'pages/category1/Index';
+import Category1 from 'pages/category1/CategoryPage1';
+import IndexUsuarios from 'pages/usuarios';
+import EditarUsuario from 'pages/usuarios/editar';
+import 'styles/globals.css';
+import 'styles/tabla.css';
+import AuthLayout from 'layouts/AuthLayout';
+import Register from 'pages/auth/register';
+
+// import PrivateRoute from 'components/PrivateRoute';
+
+// const httpLink = createHttpLink({
+//   uri: 'https://servidor-gql-mintic.herokuapp.com/graphql',
+// });
+
+const client = new ApolloClient({
+  uri: 'https://servidor-gql-mintic-developers.herokuapp.com/graphql',
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  const [userData, setUserData] = useState({});
+
+  return (
+    <ApolloProvider client={client}>
+      <UserContext.Provider value={{ userData, setUserData }}>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<PrivateLayout />}>
+              <Route path='' element={<Index />} />
+              <Route path='/usuarios' element={<IndexUsuarios />} />
+              <Route path='/proyectos' element={<IndexUsuarios />} />
+              <Route path='/usuarios/editar/:_id' element={<EditarUsuario />} />
+              <Route path='page2' element={<Page2 />} />
+              <Route path='category1' element={<IndexCategory1 />} />
+              <Route path='category1/page1' element={<Category1 />} />
+            </Route>
+            <Route path='/auth' element={<AuthLayout />}>
+              <Route path='register' element={<Register />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </UserContext.Provider>
+    </ApolloProvider>
+  );
+}
+
+export default App;
